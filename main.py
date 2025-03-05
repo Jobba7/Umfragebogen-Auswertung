d365892f4be06c85e60105e19e3a3a1c0144ddc9
Jobba7 <johannes.pforr@outlook.de>
2025-03-05 15:03:34 +0100

    Update UI text and file dialog titles for clarity in Excel processing

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def open_excel_file_dialog():
 
     # Dialog zum Auswählen einer Datei öffnen
     chosen_file_path = filedialog.askopenfilename(
-        title="Wähle eine Excel-Datei", filetypes=[("Excel-Dateien", "*.xlsx"), ("Alle Dateien", "*.*")]
+        title="Wähle eine Excel-Datei", filetypes=[("Excel-Dateien", "*.xlsx")]
     )
 
     # Wenn eine Datei ausgewählt wurde, wird der Dateipfad gespeichert
@@ -32,9 +32,10 @@ def export_processed_excel():
 
     # Dialog zum Speichern der Datei öffnen
     output_path = filedialog.asksaveasfilename(
-        title="Speichere die neue Excel-Datei",
+        title="Wähle den Speicherort für die neue Excel-Datei",
         defaultextension=".xlsx",
-        filetypes=[("Excel-Dateien", "*.xlsx"), ("Alle Dateien", "*.*")],
+        filetypes=[("Excel-Dateien", "*.xlsx")],
+        initialfile="Auswertung FSL-7.xlsx",
     )
 
     # Wenn kein Speicherort ausgewählt wurde, wird die Verarbeitung abgebrochen
@@ -63,7 +64,7 @@ def export_processed_excel():
 # Beginn des Programms
 # Hauptfenster erstellen
 main_window = tk.Tk()
-main_window.title("Excel-Dateiverarbeitung")
+main_window.title("FSL-7 Auswertung")
 
 # Maximierung deaktivieren
 main_window.resizable(False, False)
@@ -84,7 +85,7 @@ selected_file_path_entry = tk.Entry(frame, textvariable=excel_input_path, state=
 selected_file_path_entry.grid(row=0, column=1, padx=5, pady=5, sticky="ew")
 
 # Button zum Verarbeiten und Speichern der Datei
-export_results_button = tk.Button(frame, text="Verarbeiten und Speichern", command=export_processed_excel)
+export_results_button = tk.Button(frame, text="Auswerten und Speichern", command=export_processed_excel)
 export_results_button.grid(row=1, column=0, columnspan=2, padx=5, pady=5, sticky="ew")
 
 # Spaltenverhalten konfigurieren
